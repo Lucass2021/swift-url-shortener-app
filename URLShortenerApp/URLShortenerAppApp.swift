5a164ca0c6c8bd67c6db4c9843f705c45a79a0bd
Lucas Dias <lucas.allx@hotmail.com>
2026-05-28 08:58:43 -0300

    Initial Commit

---
+++ URLShortenerApp/URLShortenerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  URLShortenerAppApp.swift
+//  URLShortenerApp
+//
+//  Created by Lucas Dias on 28/05/26.
+//
+
+import SwiftUI
+
+@main
+struct URLShortenerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
